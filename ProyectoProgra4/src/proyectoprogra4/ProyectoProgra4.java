0bfa6854308f82c27254c716a8ef9328a55dd0bb
JeanSagot <jeanpaulsagotvargas@gmail.com>
2018-06-21 13:10:56 -0600

    Inicio de interfaz y chat funcionando atraves de sockets

---
+++ ProyectoProgra4/src/proyectoprogra4/ProyectoProgra4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogra4;
+
+import Interface.MainFrame;
+import Interface.MainFrameClient;
+
+/**
+ *
+ * @author jeanp
+ */
+public class ProyectoProgra4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame server = new MainFrame();
+        MainFrameClient client = new MainFrameClient();
+        
+        
+    }
+    
+}
